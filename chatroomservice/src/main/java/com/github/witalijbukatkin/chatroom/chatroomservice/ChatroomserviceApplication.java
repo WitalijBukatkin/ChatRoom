e2ff3b5013d436e8136236adcef45f024745cfce
vitalijbukatkin <vitaliy.bukatkin@gmail.com>
2020-02-21 15:27:48 +0300

    Add parent, chatservice

---
+++ chatroomservice/src/main/java/com/github/witalijbukatkin/chatroom/chatroomservice/ChatroomserviceApplication.java
@@ -0,0 +1,13 @@
+package com.github.witalijbukatkin.chatroom.chatroomservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatroomserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatroomserviceApplication.class, args);
+	}
+
+}
---
+++ chatroomservice/src/test/java/com/github/witalijbukatkin/chatroom/chatroomservice/ChatroomserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.witalijbukatkin.chatroom.chatroomservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
